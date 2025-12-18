6df75ea325c5a49f2eec818028e56e8cb296bbaa
ZombieNek <pomnin228@gmail.com>
2025-12-18 16:30:27 +0500

    ..

---
+++ To_Do_List/MauiProgram.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace To_Do_List
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Jun_Italic.ttf", "MyFontSemibold");
+                    fonts.AddFont("Jun_Regular.ttf", "MyFontRegula");
+                    
+                });
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
